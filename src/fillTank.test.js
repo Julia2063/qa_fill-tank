a87c9f6df8739a43377fb2bb6b01cc9c3e5bec4c
Julia2063 <sky206380@gmail.com.com>
2025-12-03 01:19:44 +0200

    Solution

--- src/fillTank.test.js
+++ src/fillTank.test.js
@@ -1,11 +1,125 @@
 'use strict';
 
 describe('fillTank', () => {
-  // const { fillTank } = require('./fillTank');
+  const { fillTank } = require('./fillTank');
 
-  it('should ', () => {
+  it('should fill tank', () => {
+    const customer = {
+      money: 3000,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 8,
+      },
+    };
 
+    fillTank(customer, 30, 15);
+
+    expect(customer).toEqual({
+      money: 2550,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 23,
+      },
+    });
   });
 
-  // write tests here
+  it(`should fill full tank without 'amount'`, () => {
+    const customer = {
+      money: 3000,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 8,
+      },
+    };
+
+    fillTank(customer, 30);
+
+    expect(customer).toEqual({
+      money: 2040,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 40,
+      },
+    });
+  });
+
+  it(`'amount' > 'vehicle.maxTankCapacity'`, () => {
+    const customer = {
+      money: 3000,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 8,
+      },
+    };
+
+    fillTank(customer, 30, 40);
+
+    expect(customer).toEqual({
+      money: 2040,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 40,
+      },
+    });
+  });
+
+  it(`'amount' * 'fuelPrice' > 'customer.money'`, () => {
+    const customer = {
+      money: 400,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 8,
+      },
+    };
+
+    fillTank(customer, 15, 30);
+
+    expect(customer).toEqual({
+      money: 1,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 34.6,
+      },
+    });
+  });
+
+  it(`'customer.money' / 'amount' * 'fuelPrice' < 2`, () => {
+    const customer = {
+      money: 29,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 8,
+      },
+    };
+
+    fillTank(customer, 15, 30);
+
+    expect(customer).toEqual({
+      money: 29,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 8,
+      },
+    });
+  });
+
+  it(`rounds 'fuelPrice' to two decimal places`, () => {
+    const customer = {
+      money: 3000,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 8,
+      },
+    };
+
+    fillTank(customer, 15.5567, 30);
+
+    expect(customer).toEqual({
+      money: 2533.3,
+      vehicle: {
+        maxTankCapacity: 40,
+        fuelRemains: 38,
+      },
+    });
+  });
 });
